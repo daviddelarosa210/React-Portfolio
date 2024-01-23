3ad512478f79fb5c931c89a2c18f773270e4d892
David De La Rosa <daviddelarosa@Davids-MBP.attlocal.net>
2024-01-23 16:59:19 -0600

    changed path for images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,19 +8,19 @@ function Portfolio() {
   const projects = [
     {
       title: 'Password Generator',
-      image: 'public/Screen Shot 2023-09-10 at 9.25.47 PM.png',
+      image: '/Screen Shot 2023-09-10 at 9.25.47 PM.png',
       deployedLink: 'https://daviddelarosa210.github.io/Module-3-Challange/',
       githubLink: 'https://github.com/daviddelarosa210/Password-Generator.git',
     },
     {
       title: 'Work Day Scheduler',
-      image: 'public/Work-scheduler.png',
+      image: '/Work-scheduler.png',
       deployedLink: 'https://daviddelarosa210.github.io/Work-Day-Scheduler/',
       githubLink: 'https://github.com/daviddelarosa210/Work-Day-Scheduler.git',
     },
     {
       title: 'Thunder Jamz',
-      image: 'public/Screenshot 2024-01-23 at 1.36.21 PM.png',
+      image: '/Screenshot 2024-01-23 at 1.36.21 PM.png',
       deployedLink: 'https://lillyw54.github.io/weatherbasedapp/',
       githubLink: 'https://github.com/daviddelarosa210/weatherbaseapp.git',
     },
