49acd91f01284cf9060c961389fb284cd57114ab
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-01-22 18:33:33 -0600

    added skeleton to project

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+<header>
+  <div className="flexbox-item flexbox-item-1">
+    <h1>David De La Rosa</h1>
+    <nav>
+      <a href="#about-me">About Me</a>
+      <a href="#work-title">Work</a>
+      <a href="#contact-me">Contact Me</a>
+    </nav>
+  </div>
+</header>
