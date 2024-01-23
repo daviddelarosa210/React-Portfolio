64a3f167f34e1d3b6ff7e6c600ce0c1694973883
David De La Rosa <daviddelarosa@Davids-MacBook-Pro.local>
2024-01-22 19:14:44 -0600

    fixed skeleton and added some content to Footer and Header

--- React-Portfolio/src/components/Header.jsx
+++ React-Portfolio/src/components/Header.jsx
@@ -1,12 +1,17 @@
 
+function Header() {
+  return (
+    <header>
+      <div className="flexbox-item flexbox-item-1">
+        <h1>David De La Rosa</h1>
+        <nav>
+          <a href="#about-me">About Me</a>
+          <a href="#work-title">Work</a>
+          <a href="#contact-me">Contact Me</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
-<header>
-  <div className="flexbox-item flexbox-item-1">
-    <h1>David De La Rosa</h1>
-    <nav>
-      <a href="#about-me">About Me</a>
-      <a href="#work-title">Work</a>
-      <a href="#contact-me">Contact Me</a>
-    </nav>
-  </div>
-</header>
+export default Header;
